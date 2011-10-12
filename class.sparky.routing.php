7a4a0f9a6342e96d623b075dc2ad600a9b227e5d
christoph.ackermann <info@acki.be>
2011-10-12 08:33:58 +0200

    Add a first routing enable function

--- class.sparky.routing.php
+++ class.sparky.routing.php
@@ -2,7 +2,46 @@
 
     class Routing {
     
+        private    static     $headcomment = '
+# ROUTING - DO NOT CHANGE!
+';
+        private    static     $content     = '
+RewriteEngine On
+
+RewriteCond %{REQUEST_FILENAME} !/content/*
+RewriteRule .? routing.php
+
+';
+        private    static     $footcomment = '# ENDROUTING
+';
+    
         public static function enableRouting() {
+        
+            if(is_file(dire . '.htaccess')) {
+            
+                $htaccess = file_get_contents(dire . '.htaccess');
+                
+                if(stristr($htaccess, '# ROUTING - DO NOT CHANGE!')) {
+                
+                    return false;
+                    
+                } else {
+                
+                    $cont = Routing::$headcomment . Routing::$content . Routing::$footcomment;
+                
+                    $htaccess = file_get_contents(dire . '.htaccess');
+                    
+                    if(!@file_put_contents(dire . '.htaccess', $htaccess . $cont)) {
+                        
+                        panic('File access denied. Please give write access to .htaccess file.');
+                        
+                    }
+                
+                    return true;
+                
+                }
+                
+            }
         
         }
         
